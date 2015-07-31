0161527df8ad2caba69618568e8dcdaaa83e43bd
Danilo "DMZK" Metzker <maildanilorodrigues@gmail.com>
2015-07-31 18:22:54 -0400

    Update login.php

--- examples/login.php
+++ examples/login.php
@@ -1,3 +1,8 @@
+<?php
+require_once 'lib/AuthSystem.class.php';
+$auth = new AuthSystem("localhost", "root", "", "usuarios"); /*host, username, password, database name*/
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
